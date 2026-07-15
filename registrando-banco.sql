161494cd24b645e9a323f0de19e4180a1bb6f2f5
Emily Quevedo <127437064+emilyqdrosa@users.noreply.github.com>
2026-07-15 13:28:39 -0300

    Update gasto and gasto_pagamento insert

--- registrando-banco.sql
+++ registrando-banco.sql
@@ -23,21 +23,33 @@ VALUES ('PIX'),
 
 SELECT * FROM forma_de_pagamento;
 
+-- exemplo com erro: saldo insuficiente
+INSERT INTO gasto (compra, local_de_compra, data_de_compra, id_pessoa, categoria, valor)
+VALUES  ('Brinquedo pra cachorro', 'Pet Shop', '2026-07-06', 1, 'Presente', 4000);
+
 INSERT INTO gasto (compra, local_de_compra, data_de_compra, id_pessoa, categoria, valor)
 VALUES  ('Brinquedo pra cachorro', 'Pet Shop', '2026-07-06', 1, 'Presente', 3000), 
 		('Blusa', 'Renner', '2026-06-08', 2, 'Vestuário', 6000), 
-		('Youtube Premium', 'Youtube', '2026-06-12', 3, 'Assinatura', 1700);
+		('Youtube Premium', 'Youtube', '2026-06-12', 3, 'Assinatura', 1700),
+		('Ingresso cinema', 'Cinemark', '2026-07-15', 2, 'Lazer', 1000), 
+		('Uber', 'Uber', '2026-05-29', 3, 'Transporte', 2000);
 
 SELECT * FROM gasto;
 
-INSERT INTO gasto (compra, local_de_compra, data_de_compra, id_pessoa, categoria, valor)
-VALUES  ('Ingresso cinema', 'Cinemark', '2026-07-15', 2, 'Lazer', 1000), 
-		('Uber', 'Uber', '2026-05-29', 3, 'Transporte', 2000);
+-- exemplo com erro: valores diferentes
+INSERT INTO gasto_pagamento (id_gasto, id_forma_de_pagamento, valor)
+VALUES (2, 3, 1000),
+	   (2, 2, 3000);	
+	   
+INSERT INTO gasto_pagamento (id_gasto, id_forma_de_pagamento, valor)
+VALUES (3, 1, 5000);
 
 INSERT INTO gasto_pagamento (id_gasto, id_forma_de_pagamento, valor)
-VALUES (1, 3, 1000),
-       (1, 2, 2000),	   
-	   (2, 1, 6000),
-	   (3, 5, 1700);
+VALUES (2, 3, 1000),
+       (2, 2, 2000),	   
+	   (3, 1, 6000),
+	   (4, 5, 1700),
+	   (5, 5, 1000),
+	   (6, 1, 2000);	   
 
 SELECT * FROM gasto_pagamento;
